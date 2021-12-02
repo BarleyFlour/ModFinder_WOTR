d9c5166b6c6d79b5f98ff63bf9948c50ac98b4e9
Barley <noah.schlaucher@gmail.com>
2021-12-02 21:26:41 +0100

    Stuff

--- Infrastructure/ModInstall.cs
+++ Infrastructure/ModInstall.cs
@@ -8,6 +8,7 @@ using System.IO;
 using Microsoft.VisualBasic.FileIO;
 using System.Net;
 using System.Diagnostics;
+using System.Globalization;
 
 namespace ModFinder_WOTR.Infrastructure.Loader
 {
@@ -25,16 +26,51 @@ namespace ModFinder_WOTR.Infrastructure.Loader
         public string DisplayName;
         public string Version;
     }
+    public static class test
+    {
+        public static Uri FormatUri(this string pattern, params object[] args)
+        {
+            return new Uri(string.Format(CultureInfo.InvariantCulture, pattern, args), UriKind.Relative);
+        }
+    }
     public class ModInstall
     {
         //Only for git
+
         public async static void InstallFromManagerGit(ModDetails mod)
         {
             var tempfolder = Directory.CreateDirectory(Environment.GetEnvironmentVariable("TMP") + @"\TempModFolder" + new Random().Next());
             //Download files
             {
                 Debug.Write(mod.OwnerAndRepo[0] + " " + mod.OwnerAndRepo[1]);
-                var release = await Main.client.Repository.Release.GetLatest(mod.OwnerAndRepo[0], mod.OwnerAndRepo[1]);
+                //Get latest release link
+                {
+                    var todownload = ("repos/{0}/{1}/releases/latest".FormatUri(new object[]
+                    {
+                         mod.OwnerAndRepo[0],
+                       mod.OwnerAndRepo[1]
+                    }));
+                    Infrastructure.Main.client.api
+                    Debug.Write(todownload);
+                    using (var wc = new WebClient())
+                    {
+                        wc.
+                        wc.DownloadFile(todownload.Url, tempfolder + @"\" + todownload.Name);
+                        Debug.Write(tempfolder + @"\" + todownload.Name);
+                        ZipFile.ExtractToDirectory(tempfolder + @"\" + todownload.Name, tempfolder.FullName, true);
+                        File.Delete(tempfolder + @"\" + todownload.Name);
+                    }
+                }
+                /*var task = Task.Run(() => Main.client.Repository.Release.GetLatest(mod.OwnerAndRepo[0], mod.OwnerAndRepo[1]));
+                await task.ContinueWith(t =>
+                {
+                    //do somethign with t.Result here
+                    Debug.Write(t.Result);
+                    var todownload = t.Result.Assets[0];
+                    Debug.Write(todownload.Url);*/
+
+                //});
+                /*var release = await Main.client.Repository.Release.GetLatest(mod.OwnerAndRepo[0], mod.OwnerAndRepo[1]);
                 var todownload = release.Assets.First();
                 Debug.Write(todownload.Url);
                 using (var wc = new WebClient())
@@ -43,9 +79,9 @@ namespace ModFinder_WOTR.Infrastructure.Loader
                     Debug.Write(tempfolder + @"\" + todownload.Name);
                     ZipFile.ExtractToDirectory(tempfolder + @"\" + todownload.Name, tempfolder.FullName, true);
                     File.Delete(tempfolder + @"\" + todownload.Name);
-                }
+                }*/
             }
-            string foldertoinstallto = "";
+            /*string foldertoinstallto = "";
             string foldertoinstall = "";
             var modfolder = tempfolder.EnumerateDirectories().First();
             {
@@ -67,7 +103,7 @@ namespace ModFinder_WOTR.Infrastructure.Loader
                     return;
                 }
             }
-            throw new Exception("Failed to Install Mod, Wrongly Structured ZIP?");
+            throw new Exception("Failed to Install Mod, Wrongly Structured ZIP?");*/
         }
 
         public static void InstallFromZip(string ModZipPath)
