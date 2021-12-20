647373a622c0736c45652a62b2a36d1a3fa250ae
factubsio <65080026+factubsio@users.noreply.github.com>
2021-12-20 15:02:56 +0000

    remove bad using

--- ModFinderClient/MainWindow.xaml.cs
+++ ModFinderClient/MainWindow.xaml.cs
@@ -1,26 +1,12 @@
-﻿using Newtonsoft.Json;
-using Octokit;
-using System.Diagnostics;
+﻿using System.Diagnostics;
 using System.IO;
 using System.IO.Compression;
 using System.Linq;
-using System.Net;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Controls.Primitives;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-using IdentityModel.Client;
-using System.Threading;
 using ModFinder_WOTR.Infrastructure;
-using static ModFinder_WOTR.MainWindow;
 using System;
 
 namespace ModFinder_WOTR
