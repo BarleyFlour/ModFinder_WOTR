232324768cdfe78b41d3933e414ea1c239ee944e
BarleyFlour <82173938+BarleyFlour@users.noreply.github.com>
2021-12-22 01:26:32 +0100

    asdasdasd

--- ManifestUpdater/Program.cs
+++ ManifestUpdater/Program.cs
@@ -8,130 +8,116 @@ using System.Linq;
 using NexusModsNET;
 using ManifestUpdater.Properties;
 using System.IO;
-class Program
+
+var github = new GitHubClient(new ProductHeaderValue("ModFinder_WOTR"));
+var token = Environment.GetEnvironmentVariable("BARLEY_TOKEN");
+
+github.Credentials = new Credentials(token);
+
+var nexus = NexusModsClient.Create(Environment.GetEnvironmentVariable("NEXUS_APITOKEN"), "Modfinder_WOTR", "0");
+
+var contents = Resources.internal_manifest;
+
+var details = ModFinderIO.FromString<ModListBlob>(Resources.internal_manifest);
+var tasks = new List<Task<ModDetailsInternal>>();
+
+foreach (var mod in details.m_AllMods)
 {
-    static void Main(string[] args)
+    tasks.Add(Task.Run(async () =>
     {
-        var asd = Task.Factory.StartNew(() =>
+
+        if (mod.Source == ModSource.GitHub)
         {
-            Mainn(args);
-        });
-        asd.Wait();
-    }
-    static async void Mainn(string[] args)
-    {
-        var asd = args[0];
-        var github = new GitHubClient(new ProductHeaderValue("ModFinder_WOTR"));
-        var token = Environment.GetEnvironmentVariable(asd);
+            var repo = await github.Repository.Get(mod.GithubOwner, mod.GithubRepo);
+            var allReleases = await github.Repository.Release.GetAll(mod.GithubOwner, mod.GithubRepo);
+            var latestRelease = await github.Repository.Release.GetLatest(mod.GithubOwner, mod.GithubRepo);
+            if (latestRelease.Assets.Count == 0)
+                return null;
+            var todownload = latestRelease.Assets[0];
 
-        github.Credentials = new Credentials(asd);
-
-        var nexus = NexusModsClient.Create(Environment.GetEnvironmentVariable("NEXUS_APITOKEN"), "Modfinder_WOTR", "0");
-
-        var contents = Resources.internal_manifest;
-
-        var details = ModFinderIO.FromString<ModListBlob>(Resources.internal_manifest);
-        var tasks = new List<Task<ModDetailsInternal>>();
-
-        foreach (var mod in details.m_AllMods)
-        {
-            tasks.Add(Task.Run(async () =>
+            mod.Changelog = new();
+            foreach (var release in allReleases)
             {
+                var version = ModVersion.Parse(release.TagName);
+                mod.Changelog.Add((version, release.Body.Replace("\r\n", "\n")));
+            }
+            mod.Changelog.Sort((a, b) => a.version.CompareTo(b.version));
 
-                if (mod.Source == ModSource.GitHub)
-                {
-                    var repo = await github.Repository.Get(mod.GithubOwner, mod.GithubRepo);
-                    var allReleases = await github.Repository.Release.GetAll(mod.GithubOwner, mod.GithubRepo);
-                    var latestRelease = await github.Repository.Release.GetLatest(mod.GithubOwner, mod.GithubRepo);
-                    if (latestRelease.Assets.Count == 0)
-                        return null;
-                    var todownload = latestRelease.Assets[0];
-
-                    mod.Changelog = new();
-                    foreach (var release in allReleases)
-                    {
-                        var version = ModVersion.Parse(release.TagName);
-                        mod.Changelog.Add((version, release.Body.Replace("\r\n", "\n")));
-                    }
-                    mod.Changelog.Sort((a, b) => a.version.CompareTo(b.version));
-
-                    mod.DownloadLink = todownload.BrowserDownloadUrl;
-                    mod.Description = repo.Description;
-                    mod.Latest = ModVersion.Parse(latestRelease.TagName); //This is not true???
+            mod.DownloadLink = todownload.BrowserDownloadUrl;
+            mod.Description = repo.Description;
+            mod.Latest = ModVersion.Parse(latestRelease.TagName); //This is not true???
                     return mod;
-                }
-                else if (mod.Source == ModSource.Nexus)
+        }
+        else if (mod.Source == ModSource.Nexus)
+        {
+            var nexusFactory = NexusModsFactory.New(nexus);
+            var nexusmod = await nexusFactory.CreateModsInquirer().GetMod("pathfinderwrathoftherighteous", mod.NexusModID);
+            var changelog = await nexusFactory.CreateModsInquirer().GetModChangelogs("pathfinderwrathoftherighteous", mod.NexusModID);
+            var modde = await nexusFactory.CreateModFilesInquirer().GetModFilesAsync("pathfinderwrathoftherighteous", mod.NexusModID);
+
+            var release = modde.ModFiles.Last();
+
+            if (changelog != null)
+            {
+                mod.Changelog = new();
+                foreach (var entry in changelog)
                 {
-                    var nexusFactory = NexusModsFactory.New(nexus);
-                    var nexusmod = await nexusFactory.CreateModsInquirer().GetMod("pathfinderwrathoftherighteous", mod.NexusModID);
-                    var changelog = await nexusFactory.CreateModsInquirer().GetModChangelogs("pathfinderwrathoftherighteous", mod.NexusModID);
-                    var modde = await nexusFactory.CreateModFilesInquirer().GetModFilesAsync("pathfinderwrathoftherighteous", mod.NexusModID);
+                    var version = ModVersion.Parse(entry.Key);
+                    mod.Changelog.Add((version, string.Join("\n", entry.Value)));
+                }
 
-                    var release = modde.ModFiles.Last();
+                mod.Changelog.Sort((a, b) => a.version.CompareTo(b.version));
+            }
 
-                    if (changelog != null)
-                    {
-                        mod.Changelog = new();
-                        foreach (var entry in changelog)
-                        {
-                            var version = ModVersion.Parse(entry.Key);
-                            mod.Changelog.Add((version, string.Join("\n", entry.Value)));
-                        }
-
-                        mod.Changelog.Sort((a, b) => a.version.CompareTo(b.version));
-                    }
-
-                    mod.Description = nexusmod.Description;
-                    mod.Latest = ModVersion.Parse(nexusmod.Version); //This is not true???
+            mod.Description = nexusmod.Description;
+            mod.Latest = ModVersion.Parse(nexusmod.Version); //This is not true???
                     mod.DownloadLink = @"https://www.nexusmods.com/pathfinderwrathoftherighteous/mods/" + mod.NexusModID + @"?tab=files&file_id=" + release.FileId;
 
-                    return mod;
-                }
-                return null;
-            }));
+            return mod;
         }
+        return null;
+    }));
+}
 
-        //We don't want to do console printing from inside the async tasks as they will interleave the output and it is confusing
-        //So collect the results here and print them out
-        //This will also wait for all tasks to complete
-        foreach (var mod in tasks.Select(t => t.Result).Where(r => r != null))
-        {
-            Console.WriteLine();
-            Log(mod.Name);
-            LogObj("  UniqueId: ", $"{mod.ModId.Identifier}_{mod.ModId.ModType}");
-            LogObj("  Download: ", mod.DownloadLink);
-            LogObj("  Latest: ", mod.Latest);
-        }
+//We don't want to do console printing from inside the async tasks as they will interleave the output and it is confusing
+//So collect the results here and print them out
+//This will also wait for all tasks to complete
+foreach (var mod in tasks.Select(t => t.Result).Where(r => r != null))
+{
+    Console.WriteLine();
+    Log(mod.Name);
+    LogObj("  UniqueId: ", $"{mod.ModId.Identifier}_{mod.ModId.ModType}");
+    LogObj("  Download: ", mod.DownloadLink);
+    LogObj("  Latest: ", mod.Latest);
+}
 
-        var targetUser = "BarleyFlour";
-        var targetRepo = "Modfinder_WOTR";
-        var targetFile = "ManifestUpdater/Resources/master_manifest.json";
+var targetUser = "BarleyFlour";
+var targetRepo = "Modfinder_WOTR";
+var targetFile = "ManifestUpdater/Resources/master_manifest.json";
 
-        var serializedDeets = ModFinderIO.Write(details);
-        var currentFile = await github.Repository.Content.GetAllContentsByRef(targetUser, targetRepo, targetFile, "master");
-        /*var header = "blob #{" + sizeof(serializedDeets) + "}\0";
-        var combined = header + serializedDeets;
-        var asd = System.Security.Cryptography.SHA1.
-        if (currentFile[0].Sha)*/
-        var updateFile = new UpdateFileRequest("Update the mod manifest (bot)", serializedDeets, currentFile[0].Sha, "master", true);
+var serializedDeets = ModFinderIO.Write(details);
+var currentFile = await github.Repository.Content.GetAllContentsByRef(targetUser, targetRepo, targetFile, "master");
+/*var header = "blob #{" + sizeof(serializedDeets) + "}\0";
+var combined = header + serializedDeets;
+var asd = System.Security.Cryptography.SHA1.
+if (currentFile[0].Sha)*/
+var updateFile = new UpdateFileRequest("Update the mod manifest (bot)", serializedDeets, currentFile[0].Sha, "master", true);
 
 #if DEBUG
-        File.WriteAllText(Environment.GetEnvironmentVariable("MODFINDER_LOCAL_MANIFEST"), serializedDeets);
+File.WriteAllText(Environment.GetEnvironmentVariable("MODFINDER_LOCAL_MANIFEST"), serializedDeets);
 #else
 var result = await github.Repository.Content.UpdateFile(targetUser, targetRepo, targetFile, updateFile);
 LogObj("Updated: ", result.Commit.Sha);
 #endif
 
-        void Log(string str)
-        {
-            Console.Write("[ModFinder]  ");
-            Console.WriteLine(str);
-        }
-        void LogObj(string key, object value)
-        {
-            Console.Write("[ModFinder]  ");
-            Console.Write(key.PadRight(16));
-            Console.WriteLine(value ?? "<null>");
-        }
-    }
+void Log(string str)
+{
+    Console.Write("[ModFinder]  ");
+    Console.WriteLine(str);
+}
+void LogObj(string key, object value)
+{
+    Console.Write("[ModFinder]  ");
+    Console.Write(key.PadRight(16));
+    Console.WriteLine(value ?? "<null>");
 }
